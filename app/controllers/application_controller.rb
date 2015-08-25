8fe007634c86a560e06024c416bbdc6afdb5a5a1
Mehmet Beydogan <mbbrain@gmail.com>
2015-08-25 14:16:27 +0300

    Add allowing CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
+  before_filter :set_format
 
+  def set_format
+    request.format = 'json'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
